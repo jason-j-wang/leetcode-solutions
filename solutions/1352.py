826bef2984524a1ee206d59145f4ecf0e1f5fa52
jason-j-wang <jason.wang014@gmail.com>
2025-02-13 19:44:24 -0800

    removed extra comments

--- solutions/1352.py
+++ solutions/1352.py
@@ -24,9 +24,3 @@ class ProductOfNumbers:
 # obj = ProductOfNumbers()
 # obj.add(num)
 # param_2 = obj.getProduct(k)
-
-
-# Your ProductOfNumbers object will be instantiated and called as such:
-# obj = ProductOfNumbers()
-# obj.add(num)
-# param_2 = obj.getProduct(k)
