13b4058db057ffc147a1a049f4c9d6eae7404c4d
jason-j-wang <jason.wang014@gmail.com>
2025-07-09 19:39:20 -0700

    daily question

---
+++ solutions/3440.py
@@ -0,0 +1,32 @@
+#https://leetcode.com/problems/reschedule-meetings-for-maximum-free-time-ii/description/?envType=daily-question&envId=2025-07-10
+class Solution:
+    def maxFreeTime(self, eventTime: int, startTime: List[int], endTime: List[int]) -> int:
+        largest_diff = 0
+        idx = 0
+        n = len(startTime)
+        q = [False] * n
+        t1 = 0
+        t2 = 0
+        for i in range(n):
+            if endTime[i] - startTime[i] <= t1:
+                q[i] = True
+            t1 = max(t1, startTime[i] - (0 if i == 0 else endTime[i - 1]))
+
+            if endTime[n - i - 1] - startTime[n - i - 1] <= t2:
+                q[n - i - 1] = True
+            t2 = max(
+                t2,
+                (eventTime if i == 0 else startTime[n - i])
+                - endTime[n - i - 1],
+            )
+
+        res = 0
+        for i in range(n):
+            left = 0 if i == 0 else endTime[i - 1]
+            right = eventTime if i == n - 1 else startTime[i + 1]
+            if q[i]:
+                res = max(res, right - left)
+            else:
+                res = max(res, right - left - (endTime[i] - startTime[i]))
+        return res
+        
