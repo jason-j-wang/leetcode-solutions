934f0816c52576429b24f017d08de777b78ef5ee
jason-j-wang <jason.wang014@gmail.com>
2025-08-20 18:59:49 -0700

    daily question

---
+++ solutions/1504.py
@@ -0,0 +1,17 @@
+#https://leetcode.com/problems/count-submatrices-with-all-ones/description/?envType=daily-question&envId=2025-08-21
+class Solution:
+    def numSubmat(self, mat: List[List[int]]) -> int:
+        heights = [0 for _ in range(len(mat[0]))]
+        res = 0
+        for row in mat:
+            for i, x in enumerate(row):
+                heights[i] = 0 if x == 0 else heights[i] + 1
+            stack = [[-1, 0, -1]]
+            for i, h in enumerate(heights):
+                while stack[-1][2] >= h:
+                    stack.pop()
+                j, prev, _ = stack[-1]
+                cur = prev + (i - j) * h
+                stack.append([i, cur, h])
+                res += cur
+        return res
