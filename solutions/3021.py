a5c343fbc32679e68214d918fd881eb9f813172f
jason-j-wang <jason.wang014@gmail.com>
2025-08-28 18:59:49 -0700

    daily question

---
+++ solutions/3021.py
@@ -0,0 +1,4 @@
+#https://leetcode.com/problems/alice-and-bob-playing-flower-game/description/?envType=daily-question&envId=2025-08-29
+class Solution:
+    def flowerGame(self, n: int, m: int) -> int:
+        return (m * n) // 2
