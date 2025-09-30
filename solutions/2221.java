fa6d2e49f4f1c90746a9ee7a1986e5b9dba91451
jason-j-wang <jason.wang014@gmail.com>
2025-09-29 20:24:47 -0700

    daily question

---
+++ solutions/2221.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/find-triangular-sum-of-an-array/description/?envType=daily-question&envId=2025-09-30
+class Solution {
+    public int triangularSum(int[] nums) {
+        int n = nums.length;
+
+        while (n > 1) {
+            for (int i = 0; i < n - 1; i++) {
+                nums[i] = (nums[i] + nums[i+1]) % 10;
+            }
+            n--;
+        }
+
+        return nums[0];
+    }
+}
