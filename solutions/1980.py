52781bc250ba048419cfe72ae6799bf240d29948
jason-j-wang <jason.wang014@gmail.com>
2026-03-07 21:42:09 -0800

    daily question

--- solutions/1980.py
+++ solutions/1980.py
@@ -1,22 +1,8 @@
 #https://leetcode.com/problems/find-unique-binary-string/description/?envType=daily-question&envId=2025-02-20
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        s = set(nums)
-        _, ans = self.build(s, "")
-        return ans
-
-
-    def build(self, nums, cur):
-        if len(cur) == len(nums) and cur not in nums:
-            return True, cur
-
-        if len(cur) == len(nums):
-            return False, ""
-
-        bits = ["0", "1"]
-
-        for b in bits:
-            valid, string = self.build(nums, cur + b)
-            if valid:
-                return True, string
-        return False, ""
+        ans = []
+        for i in range(len(nums)):
+            bit = nums[i][i]
+            ans.append("1" if bit == "0" else "0")
+        return "".join(ans)
